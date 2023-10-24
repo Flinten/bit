0c4925652ea76905cc953a8e210263c9cec23282
Flinten <ckflintrup@gmail.com>
2023-10-24 14:57:04 +0200

    change button text

--- my-scope/ui/button/button.composition.tsx
+++ my-scope/ui/button/button.composition.tsx
@@ -2,6 +2,6 @@ import { Button } from './button';
 
 export const BasicButton = () => {
   return (
-    <Button>Casper is nice</Button>
+    <Button>123</Button>
   );
 }
--- my-scope/ui/button/button.spec.tsx
+++ my-scope/ui/button/button.spec.tsx
@@ -4,6 +4,6 @@ import { BasicButton } from './button.composition';
 
 it('renders with the correct text', () => {
   const { getByText } = render(<BasicButton />);
-  const rendered = getByText('Casper is nice');
+  const rendered = getByText('123');
   expect(rendered).toBeTruthy();
 });
