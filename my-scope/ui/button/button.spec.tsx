363d258e560c357650e08190ac1e2723bd74240b
Flinten <ckflintrup@gmail.com>
2023-11-01 15:49:43 +0100

    .

--- my-scope/ui/button/button.spec.tsx
+++ my-scope/ui/button/button.spec.tsx
@@ -4,6 +4,6 @@ import { BasicButton } from './button.composition';
 
 it('renders with the correct text', () => {
   const { getByText } = render(<BasicButton />);
-  const rendered = getByText('casper!!!');
+  const rendered = getByText('hehe');
   expect(rendered).toBeTruthy();
 });
